d7263031b5fc63dc09c296801f0501ae0b1ff976
caldric <cvaldric@gmail.com>
2021-03-18 21:17:33 -0400

    Add onButtonClick() logic

--- password-generator/src/app/app.component.ts
+++ password-generator/src/app/app.component.ts
@@ -25,8 +25,43 @@ export class AppComponent {
   }
 
   onButtonClick() {
-    this.password = 'testpassword';
-    const { passwordLength, useLetters, useNumbers, useSymbols } = this;
-    console.table({ passwordLength, useLetters, useNumbers, useSymbols });
+    const allowedCharacters: string[] = [];
+
+    if (this.useLetters) {
+      // Add alphabetical characters to allowed characters
+      const letters = 'abcdefghijklmnopqrstuvwxyz';
+      for (const letter of letters) {
+        allowedCharacters.push(letter);
+      }
+    }
+
+    if (this.useNumbers) {
+      // Add numerical characters to allowed characters
+      const numbers = '0123456789';
+      for (const number of numbers) {
+        allowedCharacters.push(number);
+      }
+    }
+
+    if (this.useSymbols) {
+      // Add symbols to allowed characters
+      const symbols = '!@#$%^&*()-_=+[{]}|`~;:\'",<.>/?';
+      for (const symbol of symbols) {
+        allowedCharacters.push(symbol);
+      }
+    }
+
+    // Generate random password
+    const generatedPassword: string[] = [];
+    for (let i = 0; i < this.passwordLength; ++i) {
+      const randomIndex = Math.floor(Math.random() * allowedCharacters.length);
+      generatedPassword.push(allowedCharacters[randomIndex]);
+    }
+
+    this.password = generatedPassword.join('');
+
+    // // Logging purposes
+    // const { passwordLength, useLetters, useNumbers, useSymbols } = this;
+    // console.table({ passwordLength, useLetters, useNumbers, useSymbols });
   }
 }
