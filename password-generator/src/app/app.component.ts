ea0d23758d64c1b9c410f430ea0a4e075573314c
caldric <cvaldric@gmail.com>
2021-03-22 18:34:54 -0400

    Refactor into a reactive form

--- password-generator/src/app/app.component.ts
+++ password-generator/src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { FormControl, FormGroup } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
@@ -6,63 +7,38 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
-  passwordLength = 12;
-  useLowercaseLetters = true;
-  useUppercaseLetters = true;
-  useNumbers = true;
-  useSymbols = false;
-  isButtonDisabled = false;
+  passwordForm = new FormGroup({
+    passwordLength: new FormControl(12),
+    useLowercaseLetters: new FormControl(true),
+    useUppercaseLetters: new FormControl(true),
+    useNumbers: new FormControl(true),
+    useSymbols: new FormControl(false),
+  });
+
   password = '';
 
-  onChangeUseLowercaseLetters() {
-    this.useLowercaseLetters = !this.useLowercaseLetters;
-    this.checkButtonDisable();
-  }
-
-  onChangeUseUppercaseLetters() {
-    this.useUppercaseLetters = !this.useUppercaseLetters;
-    this.checkButtonDisable();
-  }
-
-  onChangeUseNumbers() {
-    this.useNumbers = !this.useNumbers;
-    this.checkButtonDisable();
-  }
-
-  onChangeUseSymbols() {
-    this.useSymbols = !this.useSymbols;
-    this.checkButtonDisable();
-  }
-
-  checkButtonDisable() {
+  onSubmit() {
     const {
-      passwordLength,
       useLowercaseLetters,
       useUppercaseLetters,
       useNumbers,
       useSymbols,
-    } = this;
-    const passwordHasLength = passwordLength > 0;
-    const atLeastOneCheckboxIsChecked =
-      useLowercaseLetters || useUppercaseLetters || useNumbers || useSymbols;
+      passwordLength,
+    } = this.passwordForm.value;
 
-    this.isButtonDisabled = !(passwordHasLength && atLeastOneCheckboxIsChecked);
-  }
-
-  onButtonClick() {
     const allowedCharacters: string[] = [];
 
     {
       const letters = 'abcdefghijklmnopqrstuvwxyz';
 
-      if (this.useLowercaseLetters) {
+      if (useLowercaseLetters) {
         // Add lowercase alphabetical characters to allowed characters
         for (const letter of letters) {
           allowedCharacters.push(letter);
         }
       }
 
-      if (this.useUppercaseLetters) {
+      if (useUppercaseLetters) {
         // Add uppercase alphabetical characters to allowed characters
         for (const letter of letters.toUpperCase()) {
           allowedCharacters.push(letter);
@@ -70,7 +46,7 @@ export class AppComponent {
       }
     }
 
-    if (this.useNumbers) {
+    if (useNumbers) {
       // Add numerical characters to allowed characters
       const numbers = '0123456789';
       for (const number of numbers) {
@@ -78,7 +54,7 @@ export class AppComponent {
       }
     }
 
-    if (this.useSymbols) {
+    if (useSymbols) {
       // Add symbols to allowed characters
       const symbols = '!@#$%^&*()-_=+[{]}|`~;:\'",<.>/?';
       for (const symbol of symbols) {
@@ -88,25 +64,11 @@ export class AppComponent {
 
     // Generate random password
     const generatedPassword: string[] = [];
-    for (let i = 0; i < this.passwordLength; ++i) {
+    for (let i = 0; i < passwordLength; ++i) {
       const randomIndex = Math.floor(Math.random() * allowedCharacters.length);
       generatedPassword.push(allowedCharacters[randomIndex]);
     }
 
     this.password = generatedPassword.join('');
-
-    // // Logging purposes
-    // const {
-    //   passwordLength,
-    //   useLowercaseLetters,
-    //   useNumbers,
-    //   useSymbols,
-    // } = this;
-    // console.table({
-    //   passwordLength,
-    //   useLowercaseLetters,
-    //   useNumbers,
-    //   useSymbols,
-    // });
   }
 }
