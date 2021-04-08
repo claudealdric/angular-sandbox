8cd040826e0c8e48e71e74c1a2b7db966ccd82e8
caldric <cvaldric@gmail.com>
2021-04-08 12:15:00 -0400

    Refactor username display

--- cards/src/app/app.component.ts
+++ cards/src/app/app.component.ts
@@ -10,19 +10,19 @@ export class AppComponent {
   posts: Post[] = [
     {
       title: 'Neat Tree',
-      username: '@nature',
+      username: 'nature',
       imageUrl: 'assets/tree.jpeg',
       content: 'Saw this awesome tree during my hike today',
     },
     {
       title: 'Snowy Mountain',
-      username: '@mountainlover',
+      username: 'mountainlover',
       imageUrl: 'assets/mountain.jpeg',
       content: 'Here is a picture of a snowy mountain',
     },
     {
       title: 'Mountain Biking',
-      username: '@biking1222',
+      username: 'biking1222',
       imageUrl: 'assets/biking.jpeg',
       content: 'Did some biking today',
     },
