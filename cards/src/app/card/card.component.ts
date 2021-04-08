656ab69504a0d0319707f461595ff739e3d8724a
caldric <cvaldric@gmail.com>
2021-04-08 11:47:45 -0400

    Refactor to make the card content dynamic

--- cards/src/app/card/card.component.ts
+++ cards/src/app/card/card.component.ts
@@ -1,11 +1,20 @@
 import { Component, OnInit } from '@angular/core'
 
+import { Card } from './card.interface'
+
 @Component({
   selector: 'app-card',
   templateUrl: './card.component.html',
   styleUrls: ['./card.component.css'],
 })
 export class CardComponent implements OnInit {
+  card: Card = {
+    title: 'Neat Tree',
+    subtitle: '@nature',
+    imgSrc: 'assets/tree.jpeg',
+    content: 'Saw this awesome tree during my hike today',
+  }
+
   constructor() {}
 
   ngOnInit(): void {}
