c9e06750be9baae03d2fecfb99a2bca5afbe131a
caldric <cvaldric@gmail.com>
2021-04-07 22:59:15 -0400

    Auto-format files using the 'format' script

--- password-generator/src/app/app.component.ts
+++ password-generator/src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component } from '@angular/core'
 
 @Component({
   selector: 'app-root',
@@ -6,12 +6,12 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
-  passwordLength = 12;
-  useLowercaseLetters = true;
-  useUppercaseLetters = true;
-  useNumbers = true;
-  useSymbols = false;
-  password = '';
+  passwordLength = 12
+  useLowercaseLetters = true
+  useUppercaseLetters = true
+  useNumbers = true
+  useSymbols = false
+  password = ''
 
   buttonIsDisabled() {
     const {
@@ -20,74 +20,74 @@ export class AppComponent {
       useUppercaseLetters,
       useNumbers,
       useSymbols,
-    } = this;
+    } = this
     return !(
       passwordLength > 0 &&
       Number.isInteger(passwordLength) &&
       (useLowercaseLetters || useUppercaseLetters || useNumbers || useSymbols)
-    );
+    )
   }
 
   onChangeUseLowercaseLetters() {
-    this.useLowercaseLetters = !this.useLowercaseLetters;
+    this.useLowercaseLetters = !this.useLowercaseLetters
   }
 
   onChangeUseUppercaseLetters() {
-    this.useUppercaseLetters = !this.useUppercaseLetters;
+    this.useUppercaseLetters = !this.useUppercaseLetters
   }
 
   onChangeUseNumbers() {
-    this.useNumbers = !this.useNumbers;
+    this.useNumbers = !this.useNumbers
   }
 
   onChangeUseSymbols() {
-    this.useSymbols = !this.useSymbols;
+    this.useSymbols = !this.useSymbols
   }
 
   onButtonClick() {
-    const allowedCharacters: string[] = [];
+    const allowedCharacters: string[] = []
 
     {
-      const letters = 'abcdefghijklmnopqrstuvwxyz';
+      const letters = 'abcdefghijklmnopqrstuvwxyz'
 
       if (this.useLowercaseLetters) {
         // Add lowercase alphabetical characters to allowed characters
         for (const letter of letters) {
-          allowedCharacters.push(letter);
+          allowedCharacters.push(letter)
         }
       }
 
       if (this.useUppercaseLetters) {
         // Add uppercase alphabetical characters to allowed characters
         for (const letter of letters.toUpperCase()) {
-          allowedCharacters.push(letter);
+          allowedCharacters.push(letter)
         }
       }
     }
 
     if (this.useNumbers) {
       // Add numerical characters to allowed characters
-      const numbers = '0123456789';
+      const numbers = '0123456789'
       for (const number of numbers) {
-        allowedCharacters.push(number);
+        allowedCharacters.push(number)
       }
     }
 
     if (this.useSymbols) {
       // Add symbols to allowed characters
-      const symbols = '!@#$%^&*()-_=+[{]}|`~;:\'",<.>/?';
+      const symbols = '!@#$%^&*()-_=+[{]}|`~;:\'",<.>/?'
       for (const symbol of symbols) {
-        allowedCharacters.push(symbol);
+        allowedCharacters.push(symbol)
       }
     }
 
     // Generate random password
-    const generatedPassword: string[] = [];
+    const generatedPassword: string[] = []
     for (let i = 0; i < this.passwordLength; ++i) {
-      const randomIndex = Math.floor(Math.random() * allowedCharacters.length);
-      generatedPassword.push(allowedCharacters[randomIndex]);
+      const randomIndex = Math.floor(Math.random() * allowedCharacters.length)
+      generatedPassword.push(allowedCharacters[randomIndex])
     }
 
-    this.password = generatedPassword.join('');
+    this.password = generatedPassword.join('')
   }
 }
--- password-generator/src/app/app.module.ts
+++ password-generator/src/app/app.module.ts
@@ -1,8 +1,8 @@
-import { NgModule } from '@angular/core';
-import { FormsModule } from '@angular/forms';
-import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core'
+import { FormsModule } from '@angular/forms'
+import { BrowserModule } from '@angular/platform-browser'
 
-import { AppComponent } from './app.component';
+import { AppComponent } from './app.component'
 
 @NgModule({
   declarations: [AppComponent],
